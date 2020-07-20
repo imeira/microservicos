bfed33cc51e59429e38dab797e498169b15776c4
Igor Meira <meira.igor@gmail.com>
2020-07-20 19:17:02 -0300

    controle do estado da compra

--- loja/src/main/java/br/com/alura/microservice/loja/repository/CompraRepository.java
+++ loja/src/main/java/br/com/alura/microservice/loja/repository/CompraRepository.java
@@ -1,5 +1,10 @@
 package br.com.alura.microservice.loja.repository;
 
+import java.util.List;
+
+import javax.persistence.LockModeType;
+
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,15 +1,21 @@
 package br.com.alura.microservice.loja.service;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import br.com.alura.microservice.loja.client.FornecedorClient;
+import br.com.alura.microservice.loja.client.TransportadorClient;
 import br.com.alura.microservice.loja.dto.CompraDTO;
+import br.com.alura.microservice.loja.dto.InfoEntregaDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 import br.com.alura.microservice.loja.dto.InfoPedidoDTO;
+import br.com.alura.microservice.loja.dto.VoucherDTO;
 import br.com.alura.microservice.loja.model.Compra;
+import br.com.alura.microservice.loja.model.CompraState;
 import br.com.alura.microservice.loja.repository.CompraRepository;
 
 @Service
@@ -19,6 +25,9 @@ public class CompraService {
 	private FornecedorClient fornecedorClient;
 	
 	@Autowired
+	private TransportadorClient transportadorClient;
+	
+	@Autowired
 	private CompraRepository compraRepository;
 	
 	@HystrixCommand(threadPoolKey = "getByIdThreadPool")
@@ -26,26 +35,50 @@ public class CompraService {
 		return compraRepository.findById(id).orElse(new Compra());
 	}
 	
+	public Compra reprocessaCompra(Long id) {
+		return null;
+	}
+	
+	public Compra cancelaCompra(Long id) {
+		return null;
+	}
+	
 	@HystrixCommand(fallbackMethod = "realizaCompraFallback",
 			threadPoolKey = "realizaCompraThreadPool")
 	public Compra realizaCompra(CompraDTO compra) {
 		
-		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
-		
-		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
-		
-		System.out.println(info.getEndereco());
-		
 		Compra compraSalva = new Compra();
+		compraSalva.setState(CompraState.RECEBIDO);
+		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+		compraRepository.save(compraSalva);
+		compra.setCompraId(compraSalva.getId());
+		
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
+		compraSalva.setState(CompraState.PEDIDO_REALIZADO);
 		compraSalva.setPedidoId(pedido.getId());
 		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
-		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+		compraRepository.save(compraSalva);
+		
+		
+		InfoEntregaDTO entregaDto = new InfoEntregaDTO();
+		entregaDto.setPedidoId(pedido.getId());
+		entregaDto.setDataParaEntrega(LocalDate.now().plusDays(pedido.getTempoDePreparo()));
+		entregaDto.setEnderecoOrigem(info.getEndereco());
+		VoucherDTO voucher = transportadorClient.reservaEntrega(entregaDto);
+		compraSalva.setState(CompraState.RESERVA_ENTREGA_REALIZADA);
+		compraSalva.setDataParaEntrega(voucher.getPrevisaoParaEntrega());
+		compraSalva.setVoucher(voucher.getNumero());
 		compraRepository.save(compraSalva);
 		
 		return compraSalva;
 	}
-	
+
 	public Compra realizaCompraFallback(CompraDTO compra) {
+		if(compra.getCompraId() != null) {
+			return compraRepository.findById(compra.getCompraId()).get();
+		}
+		
 		Compra compraFallback = new Compra();
 		compraFallback.setEnderecoDestino(compra.getEndereco().toString());
 		return compraFallback;
